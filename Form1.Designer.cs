c32c2e14fa521d4432438aad2f4c4d6883c6dad8
Lucas Affre <lucasaffre@gmail.com>
2020-06-09 16:50:47 -0300

    GCODE SENDER

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Gcode_Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbPortParity = new System.Windows.Forms.ComboBox();
+            this.cmbBaudRate = new System.Windows.Forms.ComboBox();
+            this.cmbStopBits = new System.Windows.Forms.ComboBox();
+            this.cmbDataBits = new System.Windows.Forms.ComboBox();
+            this.cmbHandshake = new System.Windows.Forms.ComboBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.Sender = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.GcodeProgress = new System.Windows.Forms.ProgressBar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnArchivo = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.nombreArchivo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(16, 30);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(121, 21);
+            this.cmbPort.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BaudRate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DataBits";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "PortParity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Handshake";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(273, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "StopBits";
+            // 
+            // cmbPortParity
+            // 
+            this.cmbPortParity.FormattingEnabled = true;
+            this.cmbPortParity.Location = new System.Drawing.Point(146, 29);
+            this.cmbPortParity.Name = "cmbPortParity";
+            this.cmbPortParity.Size = new System.Drawing.Size(121, 21);
+            this.cmbPortParity.TabIndex = 7;
+            // 
+            // cmbBaudRate
+            // 
+            this.cmbBaudRate.FormattingEnabled = true;
+            this.cmbBaudRate.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cmbBaudRate.Location = new System.Drawing.Point(16, 82);
+            this.cmbBaudRate.Name = "cmbBaudRate";
+            this.cmbBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cmbBaudRate.TabIndex = 8;
+            // 
+            // cmbStopBits
+            // 
+            this.cmbStopBits.FormattingEnabled = true;
+            this.cmbStopBits.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.cmbStopBits.Location = new System.Drawing.Point(276, 29);
+            this.cmbStopBits.Name = "cmbStopBits";
+            this.cmbStopBits.Size = new System.Drawing.Size(121, 21);
+            this.cmbStopBits.TabIndex = 9;
+            // 
+            // cmbDataBits
+            // 
+            this.cmbDataBits.FormattingEnabled = true;
+            this.cmbDataBits.Items.AddRange(new object[] {
+            "8",
+            "7",
+            "6",
+            "5"});
+            this.cmbDataBits.Location = new System.Drawing.Point(146, 82);
+            this.cmbDataBits.Name = "cmbDataBits";
+            this.cmbDataBits.Size = new System.Drawing.Size(121, 21);
+            this.cmbDataBits.TabIndex = 10;
+            // 
+            // cmbHandshake
+            // 
+            this.cmbHandshake.FormattingEnabled = true;
+            this.cmbHandshake.Location = new System.Drawing.Point(276, 82);
+            this.cmbHandshake.Name = "cmbHandshake";
+            this.cmbHandshake.Size = new System.Drawing.Size(121, 21);
+            this.cmbHandshake.TabIndex = 11;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(322, 118);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 12;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.BtnConectar_Click);
+            // 
+            // Sender
+            // 
+            this.Sender.Location = new System.Drawing.Point(16, 139);
+            this.Sender.Multiline = true;
+            this.Sender.Name = "Sender";
+            this.Sender.Size = new System.Drawing.Size(157, 74);
+            this.Sender.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Sending";
+            // 
+            // GcodeProgress
+            // 
+            this.GcodeProgress.Location = new System.Drawing.Point(179, 139);
+            this.GcodeProgress.Name = "GcodeProgress";
+            this.GcodeProgress.Size = new System.Drawing.Size(100, 23);
+            this.GcodeProgress.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(176, 123);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Progress";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(322, 147);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 17;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            // 
+            // btnArchivo
+            // 
+            this.btnArchivo.Location = new System.Drawing.Point(322, 190);
+            this.btnArchivo.Name = "btnArchivo";
+            this.btnArchivo.Size = new System.Drawing.Size(75, 23);
+            this.btnArchivo.TabIndex = 18;
+            this.btnArchivo.Text = "Archivo";
+            this.btnArchivo.UseVisualStyleBackColor = true;
+            this.btnArchivo.Click += new System.EventHandler(this.BtnArchivo_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // nombreArchivo
+            // 
+            this.nombreArchivo.Location = new System.Drawing.Point(179, 168);
+            this.nombreArchivo.Multiline = true;
+            this.nombreArchivo.Name = "nombreArchivo";
+            this.nombreArchivo.Size = new System.Drawing.Size(137, 45);
+            this.nombreArchivo.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 236);
+            this.Controls.Add(this.nombreArchivo);
+            this.Controls.Add(this.btnArchivo);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.GcodeProgress);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Sender);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.cmbHandshake);
+            this.Controls.Add(this.cmbDataBits);
+            this.Controls.Add(this.cmbStopBits);
+            this.Controls.Add(this.cmbBaudRate);
+            this.Controls.Add(this.cmbPortParity);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Gcode Sender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbPortParity;
+        private System.Windows.Forms.ComboBox cmbBaudRate;
+        private System.Windows.Forms.ComboBox cmbStopBits;
+        private System.Windows.Forms.ComboBox cmbDataBits;
+        private System.Windows.Forms.ComboBox cmbHandshake;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox Sender;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ProgressBar GcodeProgress;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnArchivo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox nombreArchivo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gcode_Sender
+{
+    public partial class Form1 : Form
+    {
+        static bool lectura;
+        static bool _continue;
+        static SerialPort _serialPort;
+        string message;
+        int countlines;
+        Thread readThread = new Thread(Read);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                cmbPort.Items.Add(s);
+            }
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                cmbPortParity.Items.Add(s);
+            }
+            foreach (string s in Enum.GetNames(typeof(Handshake)))
+            {
+                cmbHandshake.Items.Add(s);
+            }
+        }
+
+        private void BtnConectar_Click(object sender, EventArgs e)
+        {
+            btnConectar.Enabled = false;
+            btnCerrar.Enabled = false;
+
+            _serialPort = new SerialPort();
+            _serialPort.PortName = cmbPort.SelectedItem.ToString();
+            _serialPort.BaudRate = int.Parse(cmbBaudRate.SelectedItem.ToString());
+            _serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), cmbPortParity.SelectedItem.ToString(), true);
+            _serialPort.DataBits = int.Parse(cmbDataBits.SelectedItem.ToString());
+            _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cmbStopBits.SelectedItem.ToString(), true);
+            _serialPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), cmbHandshake.SelectedItem.ToString(), true);
+
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+            _serialPort.Open();
+
+            _continue = true;
+            readThread.Start();
+
+            string fileName = nombreArchivo.Text;
+
+            string text = File.ReadAllText(fileName);
+            string[] lines = text.Split('\n');
+            lines = lines.Skip(6).ToArray();
+            countlines = lines.Count();
+
+            GcodeProgress.Maximum = countlines;
+            GcodeProgress.Step = 1;
+
+            foreach (string line in lines)
+            {
+                GcodeProgress.PerformStep();
+                Sender.Text = line;
+                message = line;
+                _serialPort.WriteLine(String.Format("{0}", message));
+                do
+                {
+                    if (lectura == true)
+                    {
+                        break;
+                    }
+
+                } while (true);
+                lectura = false;
+
+            }
+            readThread.Join();
+            _serialPort.Close();
+        }
+        public static void Read()
+        {
+
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    //Console.WriteLine(message);
+                    lectura = true;
+
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            _continue = false;
+            btnCerrar.Enabled = false;
+            btnConectar.Enabled = true;
+        }
+
+        private void BtnArchivo_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                nombreArchivo.Text = openFileDialog1.FileName;
+            }
+        }
+
+        
+    }
+}
